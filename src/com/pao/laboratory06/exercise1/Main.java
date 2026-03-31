26ee780c9a0d22b2ab2ff56dd72b5881c9aad191
Tint-sabei <tssw.gena@gmail.com>
2026-03-31 09:23:17 +0300

    save my work

--- src/com/pao/laboratory06/exercise1/Main.java
+++ src/com/pao/laboratory06/exercise1/Main.java
@@ -1,3 +1,33 @@
+//package com.pao.laboratory06.exercise1;
+//
+//import java.util.Arrays;
+//import java.util.Comparator;
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        // Vezi Readme.md pentru cerințe
+//        Scanner scanner = new Scanner(System.in);
+//        String optiune = scanner.next();
+//        int numarAngajati = scanner.nextInt();
+//        Angajat[] angajati = new Angajat[numarAngajati];
+//        for (int i = 0; i < numarAngajati; i++) {
+//            angajati[i] = Angajat.citeste(scanner);
+//        }
+//        // cerinte: sorteaza in functie de optiune
+//        Comparator<Angajat> comparator = switch (optiune) {
+//            case "by_name" -> (Angajat a1, Angajat a2) -> a1.getNume().compareTo(a2.getNume());
+//            case "by_salary" -> (Angajat a1, Angajat a2) -> Double.compare(a1.getSalariu(), a2.getSalariu());
+//            case "by_salary_desc" -> (Angajat a1, Angajat a2) -> Double.compare(a2.getSalariu(), a1.getSalariu());
+//            default -> (a1, a2) -> 0; // nu sortam
+//        };
+//        Arrays.sort(angajati, comparator);
+//        for (Angajat angajat : angajati) {
+//            System.out.println(angajat);
+//        }
+//    }
+//}
+
 package com.pao.laboratory06.exercise1;
 
 import java.util.Arrays;
@@ -6,24 +36,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Vezi Readme.md pentru cerințe
+
         Scanner scanner = new Scanner(System.in);
-        String optiune = scanner.next();
-        int numarAngajati = scanner.nextInt();
-        Angajat[] angajati = new Angajat[numarAngajati];
+        int numarAngajati = scanner.nextInt(); // 1
+        Angajat[] angajati = new Angajat[numarAngajati]; // 2
         for (int i = 0; i < numarAngajati; i++) {
             angajati[i] = Angajat.citeste(scanner);
         }
-        // cerinte: sorteaza in functie de optiune
-        Comparator<Angajat> comparator = switch (optiune) {
-            case "by_name" -> (Angajat a1, Angajat a2) -> a1.getNume().compareTo(a2.getNume());
-            case "by_salary" -> (Angajat a1, Angajat a2) -> Double.compare(a1.getSalariu(), a2.getSalariu());
-            case "by_salary_desc" -> (Angajat a1, Angajat a2) -> Double.compare(a2.getSalariu(), a1.getSalariu());
+        String criterion = scanner.next(); // 3
+
+        Comparator<Angajat> comparator = switch (criterion) {
+            case "by_salary" -> (Angajat a1, Angajat a2) -> Double.compare(a1.getSalariu(), a2.getSalariu()); // part A
+            case "by_name" -> (Angajat a1, Angajat a2) -> a1.getNume().compareTo(a2.getNume()); // part B
+            case "by_salary_desc" -> (Angajat a1, Angajat a2) -> Double.compare(a2.getSalariu(), a1.getSalariu()); // part C
             default -> (a1, a2) -> 0; // nu sortam
         };
+
         Arrays.sort(angajati, comparator);
+
         for (Angajat angajat : angajati) {
             System.out.println(angajat);
         }
     }
 }
+
--- src/com/pao/laboratory06/exercise1/Test.java
+++ src/com/pao/laboratory06/exercise1/Test.java
@@ -2,29 +2,10 @@ package com.pao.laboratory06.exercise1;
 
 import com.pao.test.IOTest;
 
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
         IOTest.runParts("src/com/pao/laboratory06/exercise1/tests", Main::main);
-
-
-        System.out.println("sth: " + angajat + salary_double);
-
-        Scanner scanner = new Scanner(System.in);
-        String emp_nr = scanner.next();
-        list<angajat, salary> emp = scanner.nextInt();
-        String sort_type = scanner.next();
-        Angajat[] angajati = new Angajat[emp_nr];
-        for (int i = 0; i < emp_nr; i++) {
-            angajati[i] = Angajat.citeste(scanner);
-        }
-
-        by_salary
-
-
-
     }
 }
